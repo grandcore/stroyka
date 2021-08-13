b1264e0ad37c40c6d1116d41dfcfb12e38803aa5
unparen.github.io <you@example.com>
2021-08-13 01:55:17 -0300

    microfix

--- 0ther/OLD/backend/src/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router
-const AuthController = require('../controllers/authController')
-//const {body} = require('express-validator')
-
-router.post('/login', AuthController.login)     
-
-module.exports = router
--- 0ther/OLD/frontend/components/index.js
+++
@@ -1,8 +0,0 @@
-/*
-    Auth Components Section
-*/
-export {default as Auth} from './AuthForm/Auth';
-
-export {Navbar} from './Navbar/Navbar';
-
-export {default as MainContainer} from './MainContainer/MainContainer';
