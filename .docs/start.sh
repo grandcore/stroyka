3d1b2f89de99415e61144ba231c56b86e2f84b31
Pugavkomm <slavapugavko2@gmail.com>
2021-08-18 21:54:21 +0300

    Update

--- .docs/start.sh
+++ .docs/start.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 echo "HELLO! I'm dock generator"
-
+cd .. 
+script_dir=$(pwd)/utils
+chmod +x $script_dir/check-libs.sh
+chmod +x $script_dir/check-manifestes.sh
+$script_dir/check-libs.sh
+$script_dir/check-manifestes.sh
 
 # Запустить chack-libs.sh
 # Запустить check-manifestes.sh
