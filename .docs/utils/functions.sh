1664fee75bdfe803186fc93e8612a7a79a10d123
Pugavkomm <slavapugavko2@gmail.com>
2021-08-22 20:51:37 +0300

    Update

--- .docs/utils/functions.sh
+++ .docs/utils/functions.sh
@@ -15,9 +15,28 @@ function error_exit
 
 function find_files
 {
+    #   ----------------------------------------------------------------
+    #   Function for 
+    #       Accepts :
+    #          
+    #   ---------------------------------------------------------------- 
     current_path=${pwd}
     new_path=$1
     cd ${new_path}
     find . -name "$2"
     cd ${current_path}
+}
+
+
+function containsElement () {
+    #   ----------------------------------------------------------------
+    #   Function for 
+    #       Accepts : two arguments: 
+    #       arg1 -- value, arg2 -- array 
+    #          
+    #   ---------------------------------------------------------------- 
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
 }
